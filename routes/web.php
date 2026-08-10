a21e1585ef1eb2ec639c1d6f56c8835108647337
KhimTithsatya <khimtithsatya@gmail.com>
2026-08-10 09:17:15 +0700

    command route dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,15 @@ require_once __DIR__.'/admin.php';
 require_once __DIR__.'/user.php';
 
 // $user->token
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-
 
 Route::redirect('/','auth/login');
 
